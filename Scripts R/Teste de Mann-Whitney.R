c04f3d9fad907d0e549eb7f39a1bcac9b58f40c8
Henrique Andrade <71968343+ElirKvoth@users.noreply.github.com>
2022-07-21 01:42:05 -0300

    Correção verificação da instalação pacote rstatix
    Correção (linha 9 ) na chamada para verificar a instalação do pacote rstatix, de "dplyr" -> "rstatix".

--- Scripts R/Teste de Mann-Whitney.R
+++ Scripts R/Teste de Mann-Whitney.R
@@ -2,52 +2,52 @@
 ######################### Teste de Mann-Whitney #########################
 
 
-# Passo 1: Carregar os pacotes que serão usados
+# Passo 1: Carregar os pacotes que serÃ£o usados
 
-if(!require(dplyr)) install.packages("dplyr") # Instalação do pacote caso não esteja instalado
+if(!require(dplyr)) install.packages("dplyr") # InstalaÃ§Ã£o do pacote caso nÃ£o esteja instalado
 library(dplyr)                                # Carregamento do pacote
-if(!require(dplyr)) install.packages("rstatix") # Instalação do pacote caso não esteja instalado
+if(!require(rstatix)) install.packages("rstatix") # InstalaÃ§Ã£o do pacote caso nÃ£o esteja instalado
 library(rstatix)                                # Carregamento do pacote
 
 # Passo 2: Carregar o banco de dados
 
-# Importante: selecionar o diretório de trabalho (working directory)
+# Importante: selecionar o diretÃ³rio de trabalho (working directory)
 # Isso pode ser feito manualmente: Session > Set Working Directory > Choose Directory
-# Ou usando a linha de código abaixo:
+# Ou usando a linha de cÃ³digo abaixo:
 # setwd("C:/Users/ferna/Desktop")
 
 dados <- read.csv('Banco de Dados 3.csv', sep = ';', dec = ',') # Carregamento do arquivo csv
-View(dados)                                       # Visualização dos dados em janela separada
-glimpse(dados)                                          # Visualização de um resumo dos dados
+View(dados)                                       # VisualizaÃ§Ã£o dos dados em janela separada
+glimpse(dados)                                          # VisualizaÃ§Ã£o de um resumo dos dados
 
 
-# Passo 3: Realização do teste de Mann-Whitney
+# Passo 3: RealizaÃ§Ã£o do teste de Mann-Whitney
 
 wilcox.test(Nota_Biol ~ Posicao_Sala, data = dados)
 wilcox.test(Nota_Fis ~ Posicao_Sala, data = dados)
 wilcox.test(Nota_Hist ~ Posicao_Sala, data = dados)
 
-# Observação:
-  # O teste bicaudal é o default; caso deseje unicaudal, necessário incluir:
+# ObservaÃ§Ã£o:
+  # O teste bicaudal Ã© o default; caso deseje unicaudal, necessÃ¡rio incluir:
     # alternative = "greater" ou alternative = "less"
   # Exemplo: wilcox.test(Nota_Hist ~ Posicao_Sala, data = dados, alternative="greater")
-    # Nesse caso, o teste verificará se é a mediana do primeiro grupo é maior que a mediana do segundo
-      # O R está considerando "Frente" como primeiro grupo
+    # Nesse caso, o teste verificarÃ¡ se Ã© a mediana do primeiro grupo Ã© maior que a mediana do segundo
+      # O R estÃ¡ considerando "Frente" como primeiro grupo
 
 
-# Passo 4: Análise descritiva dos dados
+# Passo 4: AnÃ¡lise descritiva dos dados
 
 dados %>% group_by(Posicao_Sala) %>% 
   get_summary_stats(Nota_Biol, Nota_Hist, Nota_Fis, type = "median_iqr")
 
-# Dados paramétricos?
+# Dados paramÃ©tricos?
 # dados %>% group_by(Posicao_Sala) %>% 
 #  get_summary_stats(Nota_Biol, Nota_Hist, Nota_Fis, type = "mean_sd")
 
 
-# Passo 5: Visualização da distribuição
+# Passo 5: VisualizaÃ§Ã£o da distribuiÃ§Ã£o
 par(mfrow=c(1,2))
 hist(dados$Nota_Biol[dados$Posicao_Sala == "Frente"],
-     ylab="Frequência", xlab="Nota", main="Grupo Frente")
+     ylab="FrequÃªncia", xlab="Nota", main="Grupo Frente")
 hist(dados$Nota_Biol[dados$Posicao_Sala == "Fundos"],
-     ylab="Frequência", xlab="Nota", main="Grupo Fundos")
+     ylab="FrequÃªncia", xlab="Nota", main="Grupo Fundos")
